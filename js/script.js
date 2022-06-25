f8c86daacbf1bd0661f3835376c879845743a93c
Alejandro <alejandroorozcomunoz@gmail.com>
2022-06-25 00:51:04 -0500

    like

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var n = 0;
+function incrementa () {
+    n++;
+    var elemento = document.querySelector ('#like')
+    elemento.innerHTML = n;
+}  
+
+var n = 0;
+function incrementa1 () {
+    n++;
+    var elemento = document.querySelector ('#like1')
+    elemento.innerHTML = n;
+}  
+
+var n = 0;
+function incrementa2 () {
+    n++;
+    var elemento = document.querySelector ('#like2')
+    elemento.innerHTML = n;
+}  
